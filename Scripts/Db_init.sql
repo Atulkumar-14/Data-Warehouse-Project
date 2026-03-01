265d55312e7967d043167390d9b5031ea8a3158d
Atulkumar-14 <123855298+Atulkumar-14@users.noreply.github.com>
2026-03-01 16:21:36 +0530

    Initialize DataWarehouse and create schemas
    This script initializes the database and creates schemas for the DataWarehouse following Medallion Architecture.

--- Scripts/Db_init.sql
+++ Scripts/Db_init.sql
@@ -1,1 +1,34 @@
+/*
+This file is the starting point where we initialize our database , create schemas.
+our DataWarehouse follows *Medallion Architecture* thats why the schemas name is ..
+Bronze, Silver and Gold.
+*/
 
+-- Use master to create database
+USE master;
+GO
+
+-- Create database only if it does not exist
+IF NOT EXISTS (
+    SELECT 1 
+    FROM sys.databases 
+    WHERE name = 'DataWarehouse'
+)
+BEGIN
+    CREATE DATABASE DataWarehouse;
+END
+GO
+
+-- Switch to DataWarehouse
+USE DataWarehouse;
+GO
+
+-- Create schemas
+CREATE SCHEMA bronze;  -- Data loaded as it is
+GO
+
+CREATE SCHEMA silver;  -- Data got transformed
+GO
+
+CREATE SCHEMA gold;    -- Data is ready for analytics
+GO
